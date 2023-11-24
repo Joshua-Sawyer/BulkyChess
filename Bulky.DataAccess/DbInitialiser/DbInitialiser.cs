52786f4fd320a8319a5f3d89ebc0e0d8ccd35e81
Joshua-Sawyer <149957529+Joshua-Sawyer@users.noreply.github.com>
2023-11-24 15:11:09 +0000

    Upload 1

---
+++ Bulky.DataAccess/DbInitialiser/DbInitialiser.cs
@@ -0,0 +1,74 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.Models;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.DbInitialiser
+{
+    public class DbInitialiser : IDbInitialiser
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _db;
+
+        public DbInitialiser(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _db = db;
+        }
+
+        public void Initialise()
+        {
+            // migrations if they are not applied
+            try
+            {
+                if(_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+
+            // create roles if they are not created
+            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
+
+                // if roles are not created, then we will create admin user as well
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "admin@bulkychess.com",
+                    Email = "admin@bulkychess.com",
+                    Name = "Josh Sawyer",
+                    PhoneNumber = "25378355",
+                    StreetAddress = "72 Mill Lane",
+                    County = "Staffordshire",
+                    PostCode = "WS113SQ",
+                    City = "Lichfield"
+                }, "Admin123*").GetAwaiter().GetResult();
+
+                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "admin@bulkychess.com");
+                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+
+            }
+
+            return;
+
+        }
+    }
+}
---
+++ Bulky.DataAccess/Migrations/20231108124441_addCompanyRecords.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCompanyRecords : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "City", "County", "Name", "PhoneNumber", "PostCode", "StreetAddress" },
+                values: new object[,]
+                {
+                    { 1, "Lichfield", "Staffs", "Chess and Bridge", "22222222", "ab544ba", "123 Main Street" },
+                    { 2, "Bolton", "Lancs", "Chess for All", "2345678", "bo5474x", "43 Old Road" },
+                    { 3, "York", "Yorks", "The Games Shop", "34623462", "yo873us", "3 Kings Road" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
